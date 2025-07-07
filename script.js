be15a0daeebb9c281ca3f1ed24fff5ef4ce8a0f5
ryanascherr <ryanascherr@gmail.com>
2025-07-07 16:38:09 -0400

    Initial commit.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("yuuup");
+
+let rafaSkills = {
+    handShakeDeal: {
+        type: "Passive",
+        slots: 5,
+        text: "Rafa gains increased gun damage and melee damage.",
+        stats: {
+            
+        }
+    }
+}
